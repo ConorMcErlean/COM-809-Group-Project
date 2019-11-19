ba7e0a181526b3278ecc3993a55fe0ee47d50e1d
Conor <C.McErlean@protonmail.com>
2019-11-19 11:30:28 +0000

    Adding early version of Kitchen Staff Class.

---
+++ src/src/CafeApp/KitchenStaff.java
@@ -0,0 +1,20 @@
+package CafeApp;
+/*
+Created by: Conor McErlean
+Created on: 19/11/2019
+
+Kitchen Staff Class, to allow Kitchen staff to view orders and mark out of stock.
+*/
+
+public class KitchenStaff {
+   // Constructors
+   public KitchenStaff(){
+   }// Default Constructor
+
+   // Early version of method to print order to console
+   protected void viewOrders(String order){
+      System.out.println(order);
+   }//viewOrders
+
+   // Method to mark an item out of stock
+}//class
---
+++ src/src/CafeApp/TestKitchenStaff.java
@@ -0,0 +1,14 @@
+package CafeApp;/*
+Created by: Conor McErlean
+Created on: 19/11/2019
+
+Additional Comments...
+*/
+
+public class TestKitchenStaff {
+   public static void main(String[] args) {
+      String order = "1 Soda bread\n1 Egg\n1 Potato Bread";
+   KitchenStaff joe = new KitchenStaff();
+   joe.viewOrders(order);
+   }//main
+}//class
