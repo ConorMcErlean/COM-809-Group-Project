b6883f65fca0db153b35770561a8a43f1be080e2
Conor McErlean <mcerlean-c13@ulster.ac.uk>
2019-11-22 09:39:19 +0000

    Adding TillOperator Class & TestTill Operator Class created by Jennifer.

---
+++ src/src/CafeApp/TestTillOperator.java
@@ -0,0 +1,43 @@
+package CafeApp;
+
+import java.util.Scanner;
+
+/**
+ * Created by pg19coj on 21/11/2019
+ * COMMENTS ABOUT PROGRAM HERE
+ */
+public class TestTillOperator {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        int tableNumber, peopleAtTable, orderCount;
+
+        TillOperator order1 = new TillOperator();
+
+        System.out.print("Enter Table Number: ");
+        tableNumber = keyboard.nextInt();
+        System.out.print("Enter Number of people at table: ");
+        peopleAtTable = keyboard.nextInt();
+
+
+        order1.setNumAtTable(peopleAtTable);
+        order1.setTableNumber(tableNumber);
+        order1.requestOrderDetails(tableNumber, peopleAtTable);
+
+        TillOperator order2 = new TillOperator();
+
+        System.out.print("Enter Table Number: ");
+        tableNumber = keyboard.nextInt();
+        System.out.print("Enter Number of people at table: ");
+        peopleAtTable = keyboard.nextInt();
+
+
+        order2.setNumAtTable(peopleAtTable);
+        order2.setTableNumber(tableNumber);
+        order2.requestOrderDetails(tableNumber, peopleAtTable);
+
+        order2.printOrderCount();
+
+
+    }//class
+}//main
---
+++ src/src/CafeApp/TillOperator.java
@@ -0,0 +1,64 @@
+package CafeApp;
+
+import java.util.Scanner;
+
+/**
+ * Created by pg19coj on 21/11/2019
+ * Methods to take an order
+ */
+public class TillOperator {
+    public static Scanner keyboard = new Scanner(System.in);
+
+    //variables used throughout
+
+    public int numAtTable;
+    public int tableNumber;
+    private static int orderCount = 0;
+
+
+    // take Order Methods:
+
+    public void setTableNumber(int tableNo) {
+        tableNumber = tableNo;
+    }//setTableNumber
+
+    public void setNumAtTable(int peopleAtTable) {
+
+        numAtTable = peopleAtTable;
+    }
+
+    public int getTableNumber (int tableNumber) {
+        return tableNumber;
+    }//geTableNumber
+
+    public int getNumAtTable (int numAtTable) {
+        return numAtTable;
+    }//geNumAtTable
+
+
+    public void requestOrderDetails(int tableNumber, int numAtTable){
+        int person = 1;
+        String order = "";
+        System.out.print("\n");
+        System.out.println("\nTable Number: " + tableNumber + "\t\tNumber of People at Table: " + numAtTable);
+        System.out.print("Person 1's Order:  ");
+        order = keyboard.nextLine();
+        person = person + 1;
+        while (person <= numAtTable){
+            System.out.print("Person " + person + "'s Order:  ");
+            order = keyboard.nextLine();
+            person = person + 1;
+        }
+
+        orderCount = orderCount +1;
+        System.out.print("\n");
+    }//requestOrderDetails
+
+
+    public void printOrderCount (){
+        System.out.print("The number of orders is: " + orderCount);
+    }
+
+
+}//class
+
