3295502d85d5be932e00b3f8dee8220cdcc0e040
Conor <C.McErlean@protonmail.com>
2019-12-02 22:21:23 +0000

    Edit of CafeApp.java commenting to include all authors.

--- src/src/CafeApp/CafeApp.java
+++ src/src/CafeApp/CafeApp.java
@@ -1,9 +1,19 @@
-package CafeApp;/*
-Created by: Conor McErlean
-Created on: 26/11/2019
+package CafeApp;
+/*
+
+This is a command line program written for use in a cafe or restaurant, it will
+take orders, pass those orders on to the kitchen, allow reciepts to be printed
+and provide management functions for adjusting prices or adding new menu items.
+
+The following code uses the Ulster University Code Conventions.
+Created as part of COM809: Professional Software Development.
+The following work is a group project, written, edited and tested in equal parts
+by: Conor McErlean, Jennifer Coggan & Matthew McCafferty.
+Created between: 18/11/2019 & 16.12.2019.
 
 Begin the program here.
-The Class below begins the program by readying up the menu, then greeting the user.
+
+The Class below begins the program by readying up the menu & greeting the user.
 After this it opens the login system.
 */
 
